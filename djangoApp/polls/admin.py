7771790b946217f5b491c9d223ad0bcb388e3475
NoorMashal <Noordin50000@gmail.com>
2025-11-09 18:58:54 -0500

    Added documentation for admin

--- djangoApp/polls/admin.py
+++ djangoApp/polls/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin  #Tells the admin interface to add Question as an object that can later be editted.
 
 from . models import Question
 
