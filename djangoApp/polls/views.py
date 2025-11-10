adb438a4fdf99b696828e462667281d901480bb0
NoorMashal <Noordin50000@gmail.com>
2025-11-09 22:20:40 -0500

    Added index and detail htmls that query  data from the database

--- djangoApp/polls/views.py
+++ djangoApp/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 
 from .models import Question
 
@@ -11,7 +12,8 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question %s." % question_id)
